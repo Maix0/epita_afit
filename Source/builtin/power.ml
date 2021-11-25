7c5f1744bcbca6f5efc10291b6339151e33401cf
maix0 <maix522@gmail.com>
2021-11-26 00:15:28 +0100

    did some work

--- Source/builtin/power.ml
+++ Source/builtin/power.ml
@@ -7,25 +7,48 @@ open Basic_arithmetics
     @param x base
     @param n exponent
  *)
-let pow x n = let rec inner = function
+let pow x n =
+  let rec inner = function
     | 0 -> if x = 0 then failwith "0 power 0 isn't defined" else 1
     | 1 -> x
-    | n -> x * (inner (n-1))   
-    in inner n;;
+    | n -> x * inner (n - 1)
+  in
+  inner n
 
 (** Fast integer exponentiation function. Logarithmic complexity.
     @param x base
     @param n exponent
  *)
-let power x n = if n mod 2 = 0 then 
-    (power (x*x) (n/2) else x * power (x * x) (n/2 - 1);
+let power x n =
+  let rec inner a b =
+    (*Printf.printf "a=%d|b=%d" a b;*)
+    if b = 0 then if a = 0 then failwith "0 power 0 isn't defined" else 1
+    else if b = 1 then a
+    else if b = 2 then a * a
+    else if b mod 2 = 0 then inner (inner a (quot b 2)) 2
+    else a * inner (inner a (quot (b - 1) 2)) 2
+  in
+  inner x n
 
 (** Fast modular exponentiation function. Logarithmic complexity.
     @param x base
     @param n exponent
     @param m modular base
  *)
-let mod_power x n m = 0
+let mod_power x n m =
+  let rec inner a b c =
+    if a = 0 then 0
+    else if b = 0 then 1
+    else if b mod 2 = 0 then
+      let y = inner a (quot b 2) c in
+      let y = y * y mod c in
+      (y + c) mod c
+    else
+      let y = a mod c in
+      let y = y * (inner a (b - 1) c mod c) mod c in
+      (y + c) mod c
+  in
+  inner x n m
 
 (** Fast modular exponentiation function mod prime. Logarithmic complexity.
     It makes use of the Little Fermat Theorem.
--- Source/builtin/test_primes.ml
+++ Source/builtin/test_primes.ml
@@ -5,7 +5,13 @@ open Basic_arithmetics
 open Power
 
 (** Deterministic primality test *)
-let is_prime n = true
+
+let is_prime n =
+  (*Samelessly stolen from the seminar's algo correction *)
+  if n < 2 then invalid_arg "prime: not defined for integers < 2"
+  else
+    let rec prime d = d * d > n || (n mod d <> 0 && prime (d + 2)) in
+    n = 2 || (n mod 2 <> 0 && prime 3)
 
 (** Primality test based on small Fermat theorem
     @param p tested integer
